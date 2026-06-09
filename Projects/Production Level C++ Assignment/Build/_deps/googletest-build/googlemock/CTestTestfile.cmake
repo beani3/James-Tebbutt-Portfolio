ad417aa13274877a7f0d0045dfbd4363becf428f
beani3 <jamestebbuft@gmail.com>
2026-06-09 21:00:29 +0100

    removed build folder from production level c++ project and updated about me

--- Projects/Production Level C++ Assignment/Build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/james/OneDrive/Desktop/Job Application Stuff/James-Tebbutt-Portfolio/projects/Production Level C++ Assignment/build/_deps/googletest-src/googlemock
-# Build directory: C:/Users/james/OneDrive/Desktop/Job Application Stuff/James-Tebbutt-Portfolio/projects/Production Level C++ Assignment/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
